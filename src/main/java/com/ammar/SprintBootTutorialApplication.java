8d5a1cb021f3e2100e83916aa420f00d740be748
Your Name <personal@email.com>
2026-04-13 12:14:27 +0800

    init

---
+++ src/main/java/com/ammar/SprintBootTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.ammar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootTutorialApplication.class, args);
+    }
+
+}
